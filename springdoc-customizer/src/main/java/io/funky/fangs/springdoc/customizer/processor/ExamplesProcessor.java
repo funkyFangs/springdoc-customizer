77ddc99d77a18aec27cba8a9ac6e06b9d9b6a614
funkyFangs <funkyFangs@outlook.com>
2023-05-23 11:12:14 -0500

    :recycle: SC-3: update processor/customizer to serialize/deserialize examples

--- springdoc-customizer/src/main/java/io/funky/fangs/springdoc/customizer/processor/ExamplesProcessor.java
+++ springdoc-customizer/src/main/java/io/funky/fangs/springdoc/customizer/processor/ExamplesProcessor.java
@@ -2,30 +2,36 @@ package io.funky.fangs.springdoc.customizer.processor;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Sets;
+import io.funky.fangs.springdoc.customizer.annotation.ExampleDetails;
+import io.funky.fangs.springdoc.customizer.model.ExampleDetailsRecord;
+import io.funky.fangs.springdoc.customizer.model.ExampleMethodRecord;
+import io.funky.fangs.springdoc.customizer.model.ExampleTargetRecord;
+import io.funky.fangs.springdoc.customizer.model.ExampleTypeRecord;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.MirroredTypeException;
 import javax.tools.FileObject;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.Field;
+import java.util.*;
 
+import static java.util.Collections.emptyMap;
 import static javax.tools.StandardLocation.CLASS_OUTPUT;
 
 @SupportedAnnotationTypes("io.funky.fangs.springdoc.customizer.annotation.ExampleDetails")
 @SupportedSourceVersion(SourceVersion.RELEASE_19)
 public class ExamplesProcessor extends AbstractProcessor {
     private static final Set<Modifier> EXAMPLE_MODIFIERS = Sets.immutableEnumSet(Modifier.STATIC, Modifier.FINAL);
-
-    public static final String EXAMPLES_FILE_NAME = "examples.json";
-    public static final TypeReference<Map<String, Set<String>>> EXAMPLES_MAP_TYPE_REFERENCE = new TypeReference<>() {};
-    public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    @VisibleForTesting
+    static final String EXAMPLES_FILE_NAME = "examples.json";
+    private static final TypeReference<Map<String, Map<String, ExampleDetailsRecord>>> EXAMPLES_MAP_TYPE_REFERENCE = new TypeReference<>() {};
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     private FileObject examplesResourceFile;
 
@@ -46,7 +52,7 @@ public class ExamplesProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         // Read map from file
-        Map<String, Set<String>> elements;
+        Map<String, Map<String, ExampleDetailsRecord>> elements;
         try {
             elements = OBJECT_MAPPER.readValue(examplesResourceFile.getCharContent(false).toString(),
                     EXAMPLES_MAP_TYPE_REFERENCE);
@@ -62,8 +68,10 @@ public class ExamplesProcessor extends AbstractProcessor {
                 if (element.getModifiers().containsAll(EXAMPLE_MODIFIERS)
                         && element instanceof VariableElement fieldElement
                         && fieldElement.getEnclosingElement() instanceof TypeElement classElement) {
-                    elements.computeIfAbsent(classElement.getQualifiedName().toString(), ignored -> new HashSet<>())
-                            .add(fieldElement.getSimpleName().toString());
+                    var detailsRecord = convertToRecord(fieldElement.getAnnotation(ExampleDetails.class));
+
+                    elements.computeIfAbsent(classElement.getQualifiedName().toString(), ignored -> new HashMap<>())
+                            .put(fieldElement.getSimpleName().toString(), detailsRecord);
                 }
             }
         }
@@ -73,8 +81,77 @@ public class ExamplesProcessor extends AbstractProcessor {
             outputStream.write(OBJECT_MAPPER.writeValueAsBytes(elements));
         }
         catch (IOException ignored) {
+            return false;
         }
 
         return true;
+    }
+
+    public static Map<Field, ExampleDetailsRecord> getExampleFields() {
+        try {
+            var examplesMap = OBJECT_MAPPER.readValue(ExamplesProcessor.class.getResource(EXAMPLES_FILE_NAME),
+                    EXAMPLES_MAP_TYPE_REFERENCE);
+
+            var result = new HashMap<Field, ExampleDetailsRecord>();
+            for (var entry : examplesMap.entrySet()) {
+                try {
+                    var examplesClass = Class.forName(entry.getKey());
+
+                    var exampleField = entry.getValue();
+                    for (var exampleEntry : exampleField.entrySet()) {
+                        try {
+                            var field = examplesClass.getDeclaredField(exampleEntry.getKey());
+                            var details = exampleEntry.getValue();
+
+                            result.put(field, details);
+                        }
+                        catch (NoSuchFieldException ignored) {
+                            // Field could not be found, so move on
+                        }
+                    }
+                }
+                catch (ClassNotFoundException ignored) {
+                    // Class could not be found, so just move on
+                }
+            }
+
+            return Map.copyOf(result);
+        }
+        catch (IOException ignored) {
+            return emptyMap();
+        }
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored") // Method call throws exception to populate class string
+    private ExampleDetailsRecord convertToRecord(ExampleDetails details) {
+        var targetRecords = new ArrayList<ExampleTargetRecord>();
+
+        for (var target : details.targets()) {
+            var methodRecords = new ArrayList<ExampleMethodRecord>();
+
+            for (var method : target.methods()) {
+                var typeRecords = new ArrayList<ExampleTypeRecord>();
+
+                for (var type : method.types()) {
+                    var mediaTypes = List.of(type.mediaTypes());
+                    var responses = List.of(type.responses());
+
+                    typeRecords.add(new ExampleTypeRecord(type.value(), mediaTypes, responses));
+                }
+
+                methodRecords.add(new ExampleMethodRecord(method.name(), typeRecords));
+            }
+
+            try {
+                target.controller();
+            }
+            catch (MirroredTypeException exception) {
+                var controller = (TypeElement) processingEnv.getTypeUtils().asElement(exception.getTypeMirror());
+
+                targetRecords.add(new ExampleTargetRecord(controller.getQualifiedName().toString(), methodRecords));
+            }
+        }
+
+        return new ExampleDetailsRecord(details.name(), details.summary(), details.description(), targetRecords);
     }
 }
--- springdoc-customizer/src/main/java/io/funky/fangs/springdoc/customizer/utility/ExampleUtilities.java
+++ springdoc-customizer/src/main/java/io/funky/fangs/springdoc/customizer/utility/ExampleUtilities.java
@@ -1,29 +1,24 @@
 package io.funky.fangs.springdoc.customizer.utility;
 
-import com.google.common.collect.Multimap;
-import io.funky.fangs.springdoc.customizer.annotation.ExampleMethod;
-import io.funky.fangs.springdoc.customizer.annotation.ExampleType;
 import io.funky.fangs.springdoc.customizer.annotation.ExampleType.Type;
-import io.funky.fangs.springdoc.customizer.processor.ExamplesProcessor;
+import io.funky.fangs.springdoc.customizer.model.ExampleMethodRecord;
+import io.funky.fangs.springdoc.customizer.model.ExampleTypeRecord;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import jakarta.annotation.Nullable;
 import lombok.experimental.UtilityClass;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
+import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-import static com.google.common.collect.ImmutableListMultimap.toImmutableListMultimap;
-import static io.funky.fangs.springdoc.customizer.processor.ExamplesProcessor.*;
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toMap;
 import static java.util.stream.Collectors.toSet;
@@ -36,150 +31,98 @@ import static java.util.stream.Collectors.toSet;
  */
 @UtilityClass
 public class ExampleUtilities {
-    /**
-     * Determines if an {@link ExampleMethod} has any {@link Type#REQUEST} types.
-
-     * @param method the {@link ExampleMethod} to validate
-     * @return true if any {@link Type#REQUEST } types are present
-     */
-    public boolean isRequestExample(ExampleMethod method) {
+    private boolean isRequestExample(ExampleMethodRecord method) {
         return isExampleOfType(method, Type.REQUEST);
     }
 
-    /**
-     * Determines if an {@link ExampleMethod} has any {@link Type#RESPONSE} types.
-
-     * @param method the {@link ExampleMethod} to validate
-     * @return true if any {@link Type#RESPONSE} types are present
-     */
-    public boolean isResponseExample(ExampleMethod method) {
+    private boolean isResponseExample(ExampleMethodRecord method) {
         return isExampleOfType(method, Type.RESPONSE);
     }
 
-    private boolean isExampleOfType(ExampleMethod method, Type type) {
-        return Stream.of(method.types())
-                .map(ExampleType::value)
+    private boolean isExampleOfType(ExampleMethodRecord method, Type type) {
+        return method.types()
+                .stream()
+                .map(ExampleTypeRecord::type)
                 .anyMatch(type::equals);
     }
 
-    /**
-     * Determines if a {@link Method} has a {@link Parameter} which matches the example type
-     * and is a {@link RequestBody}.
-     *
-     * @param method the {@link Method} to validate
-     * @param exampleType the {@link Class} of the example
-     * @return true if the {@link Method} has an applicable {@link Parameter}, or else false
-     */
-    public boolean hasRequestParameter(Method method, Class<?> exampleType) {
+    private boolean hasRequestParameter(Method method, Class<?> exampleType) {
         return Stream.of(method.getParameters())
                 .anyMatch(parameter -> exampleType.isAssignableFrom(parameter.getType())
                         && parameter.isAnnotationPresent(RequestBody.class));
     }
 
-    /**
-     * Compares a {@link Method}'s return type against an example's type.
-     *
-     * @param method the {@link Method} to validate
-     * @param exampleType the {@link Class} of the example.
-     * @return whether example type is assignable from the return type
-     */
-    public boolean hasResponseType(Method method, Class<?> exampleType) {
+    private boolean hasResponseType(Method method, Class<?> exampleType) {
         return exampleType.isAssignableFrom(method.getReturnType());
     }
 
-    /**
-     * Constructs a {@link Multimap} of {@link ExampleMethod}s to their corresponding {@link Method}s belonging to the
-     * provided controller.
-     *
-     * @param controller the {@link Class} of the controller
-     * @param exampleMethods the {@link ExampleMethod}s to create mappings for
-     * @return a {@link Multimap} of {@link ExampleMethod}s and their corresponding {@link Method}s from the controller
-     */
-    public Multimap<ExampleMethod, Method> getControllerMethods(Class<?> controller, ExampleMethod[] exampleMethods) {
-        var exampleMethodsByName = Stream.of(exampleMethods)
-                .collect(toMap(ExampleMethod::name, Function.identity()));
+    public Map<ExampleMethodRecord, Collection<Method>> getControllerMethods(Class<?> controller, Class<?> exampleType,
+                                                                             Collection<ExampleMethodRecord> exampleMethods) {
+        var exampleMethodsByName = exampleMethods.stream()
+                .collect(toMap(ExampleMethodRecord::name, Function.identity()));
 
-        return Stream.of(controller.getMethods())
-                .filter(method -> exampleMethodsByName.containsKey(method.getName()))
-                .collect(toImmutableListMultimap(method -> exampleMethodsByName.get(method.getName()),
-                        Function.identity()));
+        var result = new HashMap<ExampleMethodRecord, Collection<Method>>();
+        for (var method : controller.getDeclaredMethods()) {
+            var name = method.getName();
+
+            Optional.ofNullable(exampleMethodsByName.get(name))
+                    .filter(exampleMethod -> isRequestExample(exampleMethod) && hasRequestParameter(method, exampleType)
+                            || isResponseExample(exampleMethod) && hasResponseType(method, exampleType))
+                    .ifPresent(exampleMethod -> result.computeIfAbsent(exampleMethod, ignored -> new ArrayList<>())
+                            .add(method));
+        }
+
+        return result;
     }
 
-    /**
-     * Creates a {@link Collection} of {@link MediaType}s based on the types specified in the {@link ExampleType} as
-     * well as the default consumes and produces media types.
-     *
-     * @param type the {@link ExampleType} containing the applicable {@link MediaType}s
-     * @return a {@link Collection} of applicable {@link MediaType}s
-     */
-    public Collection<String> getMediaTypes(ExampleType type, @Nullable String defaultConsumesMediaType,
+    public Collection<String> getMediaTypes(ExampleTypeRecord type, @Nullable String defaultConsumesMediaType,
                                             @Nullable String defaultProducesMediaType) {
-        return Stream.concat(Stream.of(type.mediaTypes()),
-                Stream.of(defaultConsumesMediaType, defaultProducesMediaType)
-                        .filter(Objects::nonNull))
+        return Stream.of(type.mediaTypes().stream(),
+                        Stream.ofNullable(defaultConsumesMediaType).filter(ignored -> type.type() == Type.REQUEST),
+                        Stream.ofNullable(defaultProducesMediaType).filter(ignored -> type.type() == Type.RESPONSE))
+                .flatMap(Function.identity())
                 .collect(toSet());
     }
 
-    /**
-     * Get the {@link Content}s from the {@link Operation} based on the targets specified in the {@link ExampleType}.
-     *
-     * @param type the details of the {@link Content}s to get
-     * @param operation the {@link Operation} to source {@link Content}s from
-     * @return the applicable {@link Content}s of the {@link Operation}
-     */
-    public Collection<Content> getContents(ExampleType type, Operation operation) {
-        return switch (type.value()) {
+    public Collection<Content> getContents(ExampleTypeRecord type, Operation operation) {
+        return switch (type.type()) {
             case REQUEST -> Optional.ofNullable(operation)
                     .map(Operation::getRequestBody)
                     // Qualified because Spring's @RequestBody is imported
                     .map(io.swagger.v3.oas.models.parameters.RequestBody::getContent)
                     .map(List::of)
                     .orElse(emptyList());
-            case RESPONSE -> {
-                var responses = Stream.of(type.responses())
-                        .map(HttpStatus::value)
-                        .map(code -> Integer.toString(code))
-                        .collect(toSet());
-
-                yield Stream.ofNullable(operation)
-                        .map(Operation::getResponses)
-                        .flatMap(apiResponses -> responses.stream()
-                                .map(apiResponses::get)
-                                .map(ApiResponse::getContent)
-                                .filter(Objects::nonNull))
-                        .toList();
-            }
+            case RESPONSE -> type.responses()
+                    .stream()
+                    .map(HttpStatus::value)
+                    .map(Object::toString)
+                    .map(operation.getResponses()::get)
+                    .filter(Objects::nonNull)
+                    .map(ApiResponse::getContent)
+                    .filter(Objects::nonNull)
+                    .toList();
         };
     }
 
-    public Collection<Field> getExampleFields() {
-        try {
-            var examplesMap = OBJECT_MAPPER.readValue(ExamplesProcessor.class.getResource(EXAMPLES_FILE_NAME),
-                    EXAMPLES_MAP_TYPE_REFERENCE);
+    private final String ROOT_PATH = "/";
 
-            var result = new ArrayList<Field>();
-            for (var entry : examplesMap.entrySet()) {
-                try {
-                    var examplesClass = Class.forName(entry.getKey());
+    public RequestMapping getRequestMapping(AnnotatedElement annotatedElement) {
+        return AnnotatedElementUtils.findMergedAnnotation(annotatedElement, RequestMapping.class);
+    }
 
-                    for (var exampleFieldName : entry.getValue()) {
-                        try {
-                            result.add(examplesClass.getDeclaredField(exampleFieldName));
-                        }
-                        catch (NoSuchFieldException ignored) {
-                            // Field could not be found, so move on
-                        }
-                    }
-                }
-                catch (ClassNotFoundException ignored) {
-                    // Class could not be found, so just move on
-                }
-            }
+    public Collection<String> getRequestMappingPaths(AnnotatedElement annotatedElement) {
+        return getRequestMappingPaths(getRequestMapping(annotatedElement));
+    }
 
-            return result;
-        }
-        catch (IOException ignored) {
-            return emptyList();
-        }
+    public Collection<String> getRequestMappingPaths(RequestMapping requestMapping) {
+        return Optional.ofNullable(requestMapping)
+                .map(RequestMapping::path)
+                .filter(paths -> paths.length > 0)
+                .map(Set::of)
+                .orElseGet(() -> Set.of(""));
+    }
+
+    public String normalizePath(String path) {
+        return path.isEmpty() ? ROOT_PATH : path;
     }
 }
